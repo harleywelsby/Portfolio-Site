9a723bf4910e97a153616fa7751e889fbd83861a
Harley Welsby <harleywelsby9@gmail.com>
2024-01-21 15:22:56 +1300

    Basic page setup & test of scroll animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,62 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useRef } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      });
+    });
+
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((element) => observer.observe(element));
+  });
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
+      <section className="hidden">
+        <h1>Test Title</h1>
+        <p>Lorem Ipsum</p>
+      </section>
     </>
   );
 }
