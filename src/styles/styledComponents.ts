d24ff297d17547a337c7f62b0e46c37eaf44585c
Harley Welsby <harleywelsby9@gmail.com>
2024-02-18 16:56:01 +1300

    Cleaner sizing for mobile header

--- src/styles/styledComponents.ts
+++ src/styles/styledComponents.ts
@@ -7,12 +7,12 @@ export const NameText = styled.h1`
   color: var(--title-orange);
   margin-bottom: 0.3rem;
   margin-top: ${isMobile ? '2.5rem' : '8.5rem'};
-  font-size: ${isMobile ? '2.5rem' : '5.5rem'};
+  font-size: ${isMobile ? '2rem' : '5.5rem'};
 `;
 
 export const RoleText = styled.p`
   margin: 0;
-  font-size: ${isMobile ? '1rem' : '2.5rem'};
+  font-size: ${isMobile ? '0.8rem' : '2.5rem'};
 `;
 
 /* Images */
@@ -21,6 +21,6 @@ export const HeadshotImage = styled.img`
   border-radius: 40%;
   margin-top: ${isMobile ? '0rem' : '0rem'};
   margin-left: ${isMobile ? '0rem' : '5rem'};
-  width: ${isMobile ? '150px' : '400px'};
-  height: ${isMobile ? '150px' : '400px'};
+  width: ${isMobile ? '120px' : '400px'};
+  height: ${isMobile ? '120px' : '400px'};
 `;
